2c722f04a876a8199c99f7d2fc9dba2a390aeac9
Chinmay487 <pchinmay04@gmail.com>
2021-12-24 14:12:44 +0530

    database connected

--- seller/views.py
+++ seller/views.py
@@ -6,14 +6,15 @@ import string
 import base64
 
 # gs://shopheaven-ccc82.appspot.com
+# gs://ecommerce-c7837.appspot.com
 
 
 
 class NewProduct(APIView):
-    # def __init__(self):
-    #     self.bucket = storage.bucket('shopheaven-ccc82.appspot.com')
-    #     db = firestore.client()
-    #     product_info = db.collection('product_info')
+    def __init__(self):
+        self.bucket = storage.bucket('ecommerce-c7837.appspot.com')
+        self.db = firestore.client()
+        self.product_info = self.db.collection('product_info')
         
     def generate_name(self,files_list):
         length = 10
@@ -26,30 +27,64 @@ class NewProduct(APIView):
         return ''.join(random.choices(chars, k=length))
 
     def post(self,request):
-        print(request.data)
-    #     doc_id = [doc.id for doc in self.db.collection('product_info').stream()]
-    #     document_name = self.generate_name(doc_id)
+                
+        key_list = list(request.data.keys())
+
+        doc_id = [doc.id for doc in self.db.collection('product_info').stream()]
+        document_name = self.generate_name(doc_id)
         
-    #     new_document = self.product_info(document_name)
-    #     new_data = dict()
-    #     new_data['title'] = request.data['title']
-    #     new_data['description'] = request.data['description']
-    #     new_data['discount_price'] = request.data['discount_price']
-    #     new_data['quantity'] = request.data['quantity']
+        new_document = self.product_info.document(document_name)
+        new_data = dict()
+                
+        for index in key_list[0:5]:
+            new_data[index] = request.data[index]
+            
+        # print(new_data)
         
-    #     new_data['id']= document_name
-    #     seller_name = 'Rushikesh'
-    #     category = 'electronics'
-    #     thumbnail_image = request.data        
-    #     image_content_type = thumbnail_image.content_type  # image/jpeg ['image','jpeg']  
-    #     temp,extension = image_content_type.split('/')
+        new_data['id']= document_name
         
+        seller_id = new_data['id']
+        category = 'furniture'
         
-    #     thumbnail_path = f'{seller_name}/{category}/thumbnail.{extension}' 
-    #     thumbnail = self.bucket.blob(thumbnail_path)
-    #     thumbnail.upload_from_string(thumbnail_image.read(),content_type = image_content_type)
+    
+        thumbnail_image = request.data['thumbnail']     
+        image_content_type = thumbnail_image.content_type  # image/jpeg ['image','jpeg']  
+        temp,extension = image_content_type.split('/')
+        thumbnail_path = f'{seller_id}/{category}/thumbnail.{extension}' 
+        thumbnail = self.bucket.blob(thumbnail_path)
+        thumbnail.upload_from_string(thumbnail_image.read(),content_type = image_content_type)
         
+        new_data['thumbnail_image'] = thumbnail_path
+        
+        new_data['images'] = list()
+        
+        for key_name in key_list[6:]:
+            image_data = request.data[key_name]
+            print(image_data)
+            image_content_type = image_data.content_type
+            temp,extension = image_content_type.split('/')
+            
+            files_list = [ 
+                file.name.replace(f'{seller_id}/{category}/','') 
+                for file in self.bucket.list_blobs() 
+                if f'{seller_id}/{category}' in file.name
+            ]
+            file_name = self.generate_name(files_list)
+            image_path = f'{seller_id}/{category}/{file_name}.{extension}'
+            new_data['images'].append(image_path)
+            new_image = self.bucket.blob(image_path)
+            new_image.upload_from_string(image_data.read(),content_type = image_content_type)
+        
+        new_document.set(new_data)
         return Response("done dana done")
+    
+    
+    
+# string = "1,2,3,4,5,6"
+# new_string = string.replace(',','-')
+# 1-2-3-4-5-6
+
+
     
             
         
