101099af607807ba1dcb95701836f9fe681cba3b
Rushiahire <rushiahire9567@gmail.com>
2022-01-02 14:02:04 +0530

    auth work in progress

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GetKeys
+from .views import GetKeys,User
 
 
 urlpatterns = [
-    path('keys',GetKeys.as_view(),name="GetKeys")
+    path('keys',GetKeys.as_view(),name="GetKeys"),
+    path('user',User.as_view(),name='user')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,32 @@
+from rest_framework import response
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from keys import firebaseConfig
+import requests
+from keys import API_KEY
 
 class GetKeys(APIView):
     def get(self,request):
         return Response(firebaseConfig)
 
+
+class User(APIView):
+    def post(self,request):
+        # print(request.data['userData']['credential']['idToken'])
+        
+        url = f"https://identitytoolkit.googleapis.com/v1/accounts:lookup"
+        payload = {
+            # 'key' : request.data['userData']['credential']['idToken']
+            "key": f"{API_KEY}={request.data['userData']['credential']['idToken']}"
+
+        }
+        headers = {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+            
+        }        
+        response = requests.post(url,headers=headers , data=payload )
+        
+        print(response.text)
+        
+        
+        return Response('user add')
