70608873ab96bb95c318a2db8ffcd0a414b293c8
Rushiahire <rushiahire9567@gmail.com>
2022-01-03 16:07:17 +0530

    some changes

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,7 @@
-from rest_framework import response
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from keys import firebaseConfig
-import requests
-from keys import API_KEY
-from firebase_admin import auth
-import json
+from firebase_admin import auth,firestore
 
 
 class GetKeys(APIView):
@@ -14,14 +10,14 @@ class GetKeys(APIView):
 
 
 class User(APIView):
+    # def __init__(self):
+    #     self.user_data = firestore.collection('user_data')
+        
+        
     def post(self,request):
         # print(request.data)
-        
         info = auth.verify_id_token(request.data['idToken'])
-        
         print(info)
-        
         # print(response.text)
-        
-        
         return Response('user add')
+
