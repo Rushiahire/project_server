7945407d838fc6b4785074eeb661ea1e2b5747ea
Rushiahire <rushiahire9567@gmail.com>
2022-01-03 21:58:06 +0530

    db changes

--- authentication/views.py
+++ authentication/views.py
@@ -10,8 +10,11 @@ class GetKeys(APIView):
 
 
 class User(APIView):
-    # def __init__(self):
-    #     self.user_data = firestore.collection('user_data')
+    def __init__(self):
+        self.db = firestore.client() 
+        self.user_data = self.db.collection('user_data')
+        
+        
         
     # UserId
     # Name
@@ -26,12 +29,28 @@ class User(APIView):
     # id_seller ? (false by default)
     # seller_id = null/None (by Default)
     
+    def addNewUser(self,user_data):
+        new_user = {
+            'user_id' : '',
+            'name' : '',
+            'email' : '',
+            'email_verified': False ,
+            'phone_number' : '',
+            'phone_number_verified' : False ,
+            'addresses' : '',
+            'cart' : '',
+            'purchase_history' : '',
+            'id_seller' : False ,
+            'seller_id' : ''
+        }
         
+        new_document = self.user_data.document('user_id')
+        new_document.set(new_user)
         
     def post(self,request):
         # print(request.data)
         info = auth.verify_id_token(request.data['idToken'])
-        print(info)
+        print(auth.list_users())    
         # print(response.text)
         return Response('user add')
 
